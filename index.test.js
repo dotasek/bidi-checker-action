fe0e19d301ef45699fd12c6fac78940b6ed8880d
dotasek <dotasek.dev@gmail.com>
2021-11-12 16:54:36 -0500

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,24 +1,24 @@
-const wait = require('./wait');
 const process = require('process');
 const cp = require('child_process');
 const path = require('path');
+const { fail } = require('assert');
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 100;
+test('test valid-project', () => {
+  process.env['GITHUB_WORKSPACE'] = './test-resources/valid-project';
   const ip = path.join(__dirname, 'index.js');
   const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
   console.log(result);
+})
+
+test('test invalid-project', () => {
+  process.env['GITHUB_WORKSPACE'] = './test-resources/invalid-project';
+  const ip = path.join(__dirname, 'index.js');
+  try {
+    cp.execSync(`node ${ip}`, {env: process.env});
+    fail('invalid project should not reach this point.');
+  } catch (e) {
+    expect(e.message).toMatch(/Command failed/);
+  }
 })
