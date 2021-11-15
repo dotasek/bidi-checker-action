dfb4b1111fa24bfd47bb3c045c9c88613f1261e7
dotasek <dotasek.dev@gmail.com>
2021-11-15 11:37:59 -0500

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -8273,23 +8273,6 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
-/***/ 4258:
-/***/ ((module) => {
-
-let wait = function (milliseconds) {
-  return new Promise((resolve) => {
-    if (typeof milliseconds !== 'number') {
-      throw new Error('milliseconds not a number');
-    }
-    setTimeout(() => resolve("done!"), milliseconds)
-  });
-};
-
-module.exports = wait;
-
-
-/***/ }),
-
 /***/ 2877:
 /***/ ((module) => {
 
@@ -8468,7 +8451,6 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const core = __nccwpck_require__(2186);
-const wait = __nccwpck_require__(4258);
 const github = __nccwpck_require__(5438);
 const path = __nccwpck_require__(5622)
 const fs = __nccwpck_require__(5747);
